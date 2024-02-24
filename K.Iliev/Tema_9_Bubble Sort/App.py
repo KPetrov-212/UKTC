36dd4e5097ca2c3ed93dbbf774016e17bf696070
Kristian Petrov <krisi.petroff@gmail.com>
2024-02-24 16:43:51 +0200

    Krasi HW

--- K.Iliev/Tema_9_Bubble Sort/App.py
+++ K.Iliev/Tema_9_Bubble Sort/App.py
@@ -1,15 +1,17 @@
-from Util import Bubble
+from Util import bubble_sort_for
+from Util import bubble_sort_while
 from numpy.random import randint
 
 n = int(input("Enter number of elements /n/: "))
 arr = list(randint(0, 100, n))
 print(f"Unsorted: {arr}")
 
-array = Bubble(arr)
-impl = input("Choose how to sort (for/while): ")
-if impl == "for":
-    array.bubble_sort_for()
-    print(f"Sorted with FOR: {arr}")
-elif impl == "while":
-    array.sortWhile()
-    print(f"Sorted with WHILE: {arr}")
+
+sorted_for = arr[:]
+sorted_while = arr[:]
+
+bubble_sort_for(sorted_for)
+print(f"Sorted with FOR: {sorted_for}")
+
+bubble_sort_while(sorted_while)
+print(f"Sorted with WHILE: {sorted_while}")
